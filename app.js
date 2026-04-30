b17821afe5e10ca3e6be5ff8ddfeba38d8688a07
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-04-30 22:01:14 +0900

    20chi xamda 23~25chi dars vazifalari bajarib tugatildi.

--- app.js
+++ app.js
@@ -1,57 +1,122 @@
+// bu 1chi urunish starting
+// console.log("Web Serverni boshlash");
+// const express = require("express");
+// const res = require("express/lib/response");
+// const app = express();
+
+// // MongoDB choqirish
+// // bu 24chi darsdagi xolat ISHLAMADI
+// // const db = require("./server").db();
+
+// // bu gpt varyanti ISHLADI
+// const { getDB } = require("./server");
+// const db = getDB();
+// const user = require("./database/user.json");
+
+// // 1: Kirish code
+// app.use(express.static("public"));
+// app.use(express.json()); // kirib kelayotgan object datani json formatga o'giribberadi
+// app.use(express.urlencoded({extended: true})); // serverga user kiritgan malumotlarni to'playdi
+
+// // 2: Session code
+// // 3: Views code
+// app.set("views", "views");
+// app.set("view engine", "ejs");
+
+// // 4: Routing code
+// app.post("/create-item", (req, res) => { //post malumotni o'zibilan olib keladi
+//     console.log("user entered /create-item");
+//     console.log(req.body);
+//     const new_reja = req.body.reja;
+//     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+//         if(err) {
+//             console.log(err);
+//             res.end('something went wrong');
+//         } else {
+//             res.end('successfully added');
+//         }
+//     });
+// })
+
+// app.get('/', function (req, res) { //get bizga malumotni o'qish uchun
+//     console.log("user entered /");
+//     db.collection("plans").find().toArray((err, data) => {
+//         if(err) {
+//             console.log(err);
+//             res.end("something went wrong");
+//         } else {
+//             res.render("reja", { items: data});
+//         }
+//     });
+// });
+
+// app.get('/author', function (req, res) {
+//     console.log("user entered /author");
+//     res.render("author", { user: user });
+// });
+
+// module.exports = app;
+// bu 1chi urunish ending
+
+// bu 2chi urunish atarting
 console.log("Web Serverni boshlash");
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
-
-// MongoDB choqirish
-// bu 24chi darsdagi xolat ISHLAMADI
-// const db = require("./server").db();
-
-// bu gpt varyanti ISHLADI
 const { getDB } = require("./server");
-const db = getDB();
 const user = require("./database/user.json");
 
 // 1: Kirish code
 app.use(express.static("public"));
-app.use(express.json()); // kirib kelayotgan object datani json formatga o'giribberadi
-app.use(express.urlencoded({extended: true})); // serverga user kiritgan malumotlarni to'playdi
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
+
 // 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.post("/create-item", (req, res) => { //post malumotni o'zibilan olib keladi
-    console.log("user entered /create-item");
-    console.log(req.body);
-    const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end('successfully added');
-        }
-    });
-})
-
-app.get('/', function (req, res) { //get bizga malumotni o'qish uchun
+app.get("/", async function (req, res) {
     console.log("user entered /");
-    db.collection("plans").find().toArray((err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.render("reja", { items: data});
-        }
-    });
+
+    try {
+    const db = getDB();
+    const data = await db.collection("plans").find().toArray();
+    res.render("reja", { items: data });
+    } catch (err) {
+        console.log("ERROR on / route:", err.message);
+        res.status(500).send("Something went wrong");
+    }
 });
 
-app.get('/author', function (req, res) {
+// Create item
+app.post("/create-item", async function (req, res) {
+    console.log("user entered /create-item");
+    console.log(req.body);
+
+    try {
+        const db = getDB();
+        const newReja = req.body.reja;
+        if (!newReja) {
+            return res.status(400).send("Reja kiritilmadi");
+        }
+
+        await db.collection("plans").insertOne({ reja: newReja });
+
+        res.send("successfully added");
+    } catch (err) {
+            console.log("ERROR on /create-item:", err.message);
+            res.status(500).send("Something went wrong");
+        }
+});
+
+// Author page
+app.get("/author", function (req, res) {
     console.log("user entered /author");
-    res.render("author", { user: user });
+
+    res.render("author", { user });
 });
 
 module.exports = app;
+// bu 2chi urunish ending
--- server.js
+++ server.js
@@ -30,50 +30,188 @@
 // );
 
 
-
+// bu 2chi usul starting
 // bu gpt varyanti ISHLADI
+// const http = require("http");
+// const { MongoClient } = require("mongodb");
+// const fs = require("fs");
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//     if(err) {
+//         console.log("ERROR", err);
+//     }else {
+//         user = JSON.parse(data)
+//     }
+// });
+
+// const connectionString =
+//   "mongodb+srv://LEO:leo12345678@cluster0.d4zhpe5.mongodb.net/Reja";
+// const client = new MongoClient(connectionString);
+// let db;
+// async function startServer() {
+//   console.log("Server boshlanmoqda...");
+//   try {
+//     console.log("MongoDB ga ulanish...");
+//     await client.connect();
+//     console.log("MongoDB connection succeed");
+//     db = client.db(); // MUHIM
+//     const app = require("./app");
+//     const server = http.createServer(app);
+//     let PORT = 3000;
+//     server.listen(PORT, function () {
+//       console.log(`The server is running successfully on port: http://localhost:${PORT}`);
+//     });
+//   } catch (err) {
+//     console.log("ERROR on connection MongoDB", err);
+//   }
+// }
+
+// // EXPORT QILAMIZ
+// function getDB() {
+//   return db;
+// }
+
+// module.exports = { startServer, getDB };
+
+// // serverni ishga tushur
+// startServer();
+// bu 2chi usul ending
+
+
+
+// bu 3chi usul starting
+// const http = require("http");
+// const { MongoClient } = require("mongodb");
+// const fs = require("fs");
+
+// let user;
+
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR reading user.json:", err);
+//   } else {
+//     user = JSON.parse(data);
+//   }
+// });
+
+// const connectionString =
+//   "mongodb+srv://LEO:leo12345678@cluster0.d4zhpe5.mongodb.net/Reja?retryWrites=true&w=majority";
+
+// const client = new MongoClient(connectionString, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// let db;
+
+// async function startServer() {
+//   console.log("Server boshlanmoqda...");
+
+//   try {
+//     console.log("MongoDB ga ulanish...");
+
+//     await client.connect();
+
+//     console.log("MongoDB connection succeed");
+
+//     db = client.db("Reja");
+
+//     const app = require("./app");
+//     const server = http.createServer(app);
+
+//     const PORT = 3000;
+
+//     server.listen(PORT, function () {
+//       console.log(
+//         `The server is running successfully on port: http://localhost:${PORT}`,
+//       );
+//     });
+//   } catch (err) {
+//     console.log("ERROR on connection MongoDB", err);
+//   }
+// }
+
+// function getDB() {
+//   if (!db) {
+//     throw new Error("Database hali ulanmagan");
+//   }
+
+//   return db;
+// }
+
+// module.exports = { startServer, getDB };
+
+// startServer();
+// bu 3chi usul ending
+
+// bu 4chi usul starting
 const http = require("http");
 const { MongoClient } = require("mongodb");
 const fs = require("fs");
 
+const connectionString =
+  "mongodb+srv://LEO:leo12345678@cluster0.d4zhpe5.mongodb.net/Reja?retryWrites=true&w=majority";
+
+const client = new MongoClient(connectionString);
+
+let db;
 let user;
+
+// user.json o‘qish
 fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR", err);
-    }else {
-        user = JSON.parse(data)
+  if (err) {
+    console.log("ERROR reading user.json:", err.message);
+  } else {
+    try {
+      user = JSON.parse(data);
+      console.log("user.json successfully loaded");
+    } catch (parseErr) {
+      console.log("ERROR parsing user.json:", parseErr.message);
     }
+  }
 });
 
-const connectionString =
-  "mongodb+srv://LEO:leo12345678@cluster0.d4zhpe5.mongodb.net/Reja";
-const client = new MongoClient(connectionString);
-let db;
 async function startServer() {
   console.log("Server boshlanmoqda...");
+
   try {
     console.log("MongoDB ga ulanish...");
     await client.connect();
+
+    db = client.db("Reja");
+
     console.log("MongoDB connection succeed");
-    db = client.db(); // MUHIM
+    // console.log("Web Serverni boshlash");
+
     const app = require("./app");
     const server = http.createServer(app);
-    let PORT = 3000;
+
+    const PORT = 3000;
+
     server.listen(PORT, function () {
-      console.log(`The server is running successfully on port: http://localhost:${PORT}`);
+      console.log(
+        `The server is running successfully on port: http://localhost:${PORT}`,
+      );
     });
   } catch (err) {
-    console.log("ERROR on connection MongoDB", err);
+    console.log("ERROR on connection MongoDB:", err.message);
   }
 }
 
-// EXPORT QILAMIZ
 function getDB() {
+  if (!db) {
+    throw new Error("Database hali ulanmagan!");
+  }
+
   return db;
 }
 
-module.exports = { startServer, getDB };
+function getUser() {
+  return user;
+}
 
-// serverni ishga tushur
+module.exports = { startServer, getDB, getUser };
+
 startServer();
-
+// bu 4chi usul ending
